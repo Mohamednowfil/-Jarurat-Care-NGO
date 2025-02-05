7619987bdea7c468706208fa0744ca6257d63c12
Mohamednowfil <mohamednowfil23@gmail.com>
2025-02-05 13:41:05 +0530

    Map error fix

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -1,42 +1,16 @@
-// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-// import "leaflet/dist/leaflet.css";
-
-// const MapComponent = () => {
-//   const position = [12.9716, 77.5946]; 
-
-//   return (
-//     <div className="map-container">
-//       <MapContainer center={position} zoom={13} scrollWheelZoom={false} className="leaflet-map">
-//         <TileLayer
-//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//         />
-//         <Marker position={position}>
-//           <Popup> This is Bangalore, India. </Popup>
-//         </Marker>
-//       </MapContainer>
-//     </div>
-//   );
-// };
-
-// export default MapComponent;
-
-
-
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
-import locationIcon from "./location.png"; // Replace with your image path
+import locationIcon from "./location.png"; 
 
 const MapComponent = () => {
-  const position = [12.9716, 77.5946]; // Bangalore coordinates
+  const position = [12.9716, 77.5946]; 
 
-  // Custom Marker Icon
   const customIcon = L.icon({
-    iconUrl: locationIcon, // Path to your image
-    iconSize: [40, 40], // Size of the icon [width, height]
-    iconAnchor: [20, 40], // Position where the icon is anchored
-    popupAnchor: [0, -40], // Position of the popup relative to the icon
+    iconUrl: locationIcon, 
+    iconSize: [40, 40], 
+    iconAnchor: [20, 40], 
+    popupAnchor: [0, -40], 
   });
 
   return (
